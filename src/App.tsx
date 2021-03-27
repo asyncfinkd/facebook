5c116cd161268353efbae9b3ee53fce31815ae65
Nika Shamiladze <zuckdeveloper@gmail.com>
2021-03-27 12:41:44 +0400

    :boom: We Started Building Facebook :fire: Clone

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Hero } from "./components/hero/Hero";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Hero: React.FC = () => {
+    return(
+        <>
+            <p>Hero Component</p>
+        </>
+    )
+}
