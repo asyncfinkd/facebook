1b88a55d7c6c3a009d0396831389b2770dc149f6
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-03 11:24:49 +0400

    Navbar Add Component (full)

--- src/components/home/navbar/navbarAddComponent/NavbarAddComponent.tsx
+++ src/components/home/navbar/navbarAddComponent/NavbarAddComponent.tsx
@@ -13,13 +13,20 @@ export const NavbarAddComponent: React.FC = () => {
           border: "1px solid #474a4d",
           borderRadius: "8px",
           right: "0",
-          padding: "1rem",
+          padding: "0.5rem",
           overflow: "hidden",
           transition: "height 500ms ease",
         }}
       >
         <div style={{ width: "100%" }}>
-          <h1 style={{ fontSize: "1.5rem", color: "#E4E6EB", fontWeight: 600 }}>
+          <h1
+            style={{
+              fontSize: "1.5rem",
+              color: "#E4E6EB",
+              fontWeight: 600,
+              padding: "5px 10px",
+            }}
+          >
             Create
           </h1>
           <div
@@ -29,7 +36,6 @@ export const NavbarAddComponent: React.FC = () => {
               display: "flex",
               alignItems: "center",
               borderRadius: "8px",
-              transition: "background 500ms",
               padding: "0.5rem",
             }}
           >
@@ -86,6 +92,418 @@ export const NavbarAddComponent: React.FC = () => {
                   }}
                 >
                   Share a post on News Feed.
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className="create__animation"
+            style={{
+              height: "50px",
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "8px",
+              padding: "0.5rem",
+            }}
+          >
+            <div
+              style={{
+                width: "36px",
+                height: "36px",
+                backgroundColor: "rgba(255,255,255,0.1)",
+                borderRadius: "50%",
+                position: "relative",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <i
+                style={{
+                  backgroundPosition: "0 -682px",
+                  backgroundSize: "auto",
+                  backgroundRepeat: "no-repeat",
+                  display: "inline-block",
+                  height: "20px",
+                  width: "20px",
+                  backgroundImage:
+                    "url('https://static.xx.fbcdn.net/rsrc.php/v3/yt/r/UGf0DxEk6Mf.png')",
+                  filter: "invert(100%)",
+                }}
+              ></i>
+            </div>
+            <div style={{ marginLeft: "10px" }}>
+              <div style={{ lineHeight: "0", marginTop: "9px" }}>
+                <span
+                  style={{
+                    color: "#E4E6EB",
+                    fontSize: ".9375rem",
+                    textAlign: "left",
+                    fontWeight: 500,
+                    maxWidth: "100%",
+                  }}
+                >
+                  Story
+                </span>
+              </div>
+              <div
+                style={{
+                  marginTop: "5px",
+                }}
+              >
+                <span
+                  style={{
+                    color: "#B0B3B8",
+                    fontWeight: "normal",
+                    fontSize: ".8125rem",
+                  }}
+                >
+                  Share a photo or write something.
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className="create__animation"
+            style={{
+              height: "50px",
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "8px",
+              padding: "0.5rem",
+            }}
+          >
+            <div
+              style={{
+                width: "36px",
+                height: "36px",
+                backgroundColor: "rgba(255,255,255,0.1)",
+                borderRadius: "50%",
+                position: "relative",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <i
+                style={{
+                  backgroundPosition: "0 -271px",
+                  backgroundSize: "auto",
+                  backgroundRepeat: "no-repeat",
+                  display: "inline-block",
+                  height: "20px",
+                  width: "20px",
+                  backgroundImage:
+                    "url('https://static.xx.fbcdn.net/rsrc.php/v3/yS/r/OUHRTvi_Etj.png')",
+                  filter: "invert(100%)",
+                }}
+              ></i>
+            </div>
+            <div style={{ marginLeft: "10px" }}>
+              <div style={{ lineHeight: "0", marginTop: "9px" }}>
+                <span
+                  style={{
+                    color: "#E4E6EB",
+                    fontSize: ".9375rem",
+                    textAlign: "left",
+                    fontWeight: 500,
+                    maxWidth: "100%",
+                  }}
+                >
+                  Room
+                </span>
+              </div>
+              <div
+                style={{
+                  marginTop: "5px",
+                }}
+              >
+                <span
+                  style={{
+                    color: "#B0B3B8",
+                    fontWeight: "normal",
+                    fontSize: ".8125rem",
+                  }}
+                >
+                  Video chat with anyone, on or off Facebook.
+                </span>
+              </div>
+            </div>
+          </div>
+          <hr
+            style={{
+              backgroundColor: "#dadde1",
+              height: "1px",
+              color: "#dadde1",
+              borderWidth: "0",
+              marginRight: "4px",
+              marginLeft: "4px",
+            }}
+          />
+          <div
+            className="create__animation"
+            style={{
+              height: "50px",
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "8px",
+              padding: "0.5rem",
+            }}
+          >
+            <div
+              style={{
+                width: "36px",
+                height: "36px",
+                backgroundColor: "rgba(255,255,255,0.1)",
+                borderRadius: "50%",
+                position: "relative",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <i
+                style={{
+                  backgroundPosition: "-50px -73px",
+                  backgroundSize: "auto",
+                  backgroundRepeat: "no-repeat",
+                  display: "inline-block",
+                  height: "20px",
+                  width: "20px",
+                  backgroundImage:
+                    "url('https://static.xx.fbcdn.net/rsrc.php/v3/yu/r/eC_CYctKw8N.png')",
+                  filter: "invert(100%)",
+                }}
+              ></i>
+            </div>
+            <div style={{ marginLeft: "10px" }}>
+              <div style={{ lineHeight: "0", marginTop: "9px" }}>
+                <span
+                  style={{
+                    color: "#E4E6EB",
+                    fontSize: ".9375rem",
+                    textAlign: "left",
+                    fontWeight: 500,
+                    maxWidth: "100%",
+                  }}
+                >
+                  Page
+                </span>
+              </div>
+              <div
+                style={{
+                  marginTop: "5px",
+                }}
+              >
+                <span
+                  style={{
+                    color: "#B0B3B8",
+                    fontWeight: "normal",
+                    fontSize: ".8125rem",
+                  }}
+                >
+                  Connect and share with customers or fans.
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className="create__animation"
+            style={{
+              height: "50px",
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "8px",
+              padding: "0.5rem",
+            }}
+          >
+            <div
+              style={{
+                width: "36px",
+                height: "36px",
+                backgroundColor: "rgba(255,255,255,0.1)",
+                borderRadius: "50%",
+                position: "relative",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <i
+                style={{
+                  backgroundPosition: "-25px -103px",
+                  backgroundSize: "auto",
+                  backgroundRepeat: "no-repeat",
+                  display: "inline-block",
+                  height: "20px",
+                  width: "20px",
+                  backgroundImage:
+                    "url('https://static.xx.fbcdn.net/rsrc.php/v3/yD/r/C16hQdMjVph.png')",
+                  filter: "invert(100%)",
+                }}
+              ></i>
+            </div>
+            <div style={{ marginLeft: "10px" }}>
+              <div style={{ lineHeight: "0", marginTop: "9px" }}>
+                <span
+                  style={{
+                    color: "#E4E6EB",
+                    fontSize: ".9375rem",
+                    textAlign: "left",
+                    fontWeight: 500,
+                    maxWidth: "100%",
+                  }}
+                >
+                  Ad
+                </span>
+              </div>
+              <div
+                style={{
+                  marginTop: "5px",
+                }}
+              >
+                <span
+                  style={{
+                    color: "#B0B3B8",
+                    fontWeight: "normal",
+                    fontSize: ".8125rem",
+                  }}
+                >
+                  Advertise your business.
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className="create__animation"
+            style={{
+              height: "50px",
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "8px",
+              padding: "0.5rem",
+            }}
+          >
+            <div
+              style={{
+                width: "36px",
+                height: "36px",
+                backgroundColor: "rgba(255,255,255,0.1)",
+                borderRadius: "50%",
+                position: "relative",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <i
+                style={{
+                  backgroundPosition: "0 -229px",
+                  backgroundSize: "auto",
+                  backgroundRepeat: "no-repeat",
+                  display: "inline-block",
+                  height: "20px",
+                  width: "20px",
+                  backgroundImage:
+                    "url('https://static.xx.fbcdn.net/rsrc.php/v3/yS/r/OUHRTvi_Etj.png')",
+                  filter: "invert(100%)",
+                }}
+              ></i>
+            </div>
+            <div style={{ marginLeft: "10px" }}>
+              <div style={{ lineHeight: "0", marginTop: "9px" }}>
+                <span
+                  style={{
+                    color: "#E4E6EB",
+                    fontSize: ".9375rem",
+                    textAlign: "left",
+                    fontWeight: 500,
+                    maxWidth: "100%",
+                  }}
+                >
+                  Group
+                </span>
+              </div>
+              <div
+                style={{
+                  marginTop: "5px",
+                }}
+              >
+                <span
+                  style={{
+                    color: "#B0B3B8",
+                    fontWeight: "normal",
+                    fontSize: ".8125rem",
+                  }}
+                >
+                  Connect with people who share your interests.
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className="create__animation"
+            style={{
+              height: "50px",
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "8px",
+              padding: "0.5rem",
+            }}
+          >
+            <div
+              style={{
+                width: "36px",
+                height: "36px",
+                backgroundColor: "rgba(255,255,255,0.1)",
+                borderRadius: "50%",
+                position: "relative",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <i
+                style={{
+                  backgroundPosition: "0 -159px",
+                  backgroundSize: "auto",
+                  backgroundRepeat: "no-repeat",
+                  display: "inline-block",
+                  height: "20px",
+                  width: "20px",
+                  backgroundImage:
+                    "url('https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/WtM2L1oWuuk.png')",
+                  filter: "invert(100%)",
+                }}
+              ></i>
+            </div>
+            <div style={{ marginLeft: "10px" }}>
+              <div style={{ lineHeight: "0", marginTop: "9px" }}>
+                <span
+                  style={{
+                    color: "#E4E6EB",
+                    fontSize: ".9375rem",
+                    textAlign: "left",
+                    fontWeight: 500,
+                    maxWidth: "100%",
+                  }}
+                >
+                  Event
+                </span>
+              </div>
+              <div
+                style={{
+                  marginTop: "5px",
+                }}
+              >
+                <span
+                  style={{
+                    color: "#B0B3B8",
+                    fontWeight: "normal",
+                    fontSize: ".8125rem",
+                  }}
+                >
+                  Bring people together with a public or private.
                 </span>
               </div>
             </div>
