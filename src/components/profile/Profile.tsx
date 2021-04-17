51c893895f9e9bd4479e6683d18c5b35ec684707
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-17 17:22:30 +0400

    Authentication is not logged in profile.

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export const Profile: React.FC = () => {
+  const [user, setUser] = useState<any>(
+    JSON.parse(localStorage.getItem("user") || "{}")
+  );
+  const history = useHistory();
   return (
     <>
-      <p>Hello</p>
+      {localStorage.getItem("logged") != "true" ||
+      !user.username ||
+      !user.lastname ||
+      !user.username ? (
+        history.push("/")
+      ) : (
+        <>
+          <p>hello fb</p>
+        </>
+      )}
     </>
   );
 };
