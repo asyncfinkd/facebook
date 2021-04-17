dd51620e5a054191935955d8cc58b5f553a0ba40
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-17 17:18:11 +0400

    We Started Profile Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Hero } from "./components/hero/Hero";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Home } from "./components/home/Home";
+import { Profile } from "./components/profile/Profile";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <BrowserRouter>
         <Route path="/" exact component={Hero} />
         <Route path="/feed" exact component={Home} />
+        <Route path="/profile" exact component={Profile} />
       </BrowserRouter>
     </>
   );
---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Profile: React.FC = () => {
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  );
+};
